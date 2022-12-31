bfca915241a12094c7c74fbd3e4cf600c12e1e4b
Harshil Chudasama <harshilc71@gmail.com>
2023-01-01 02:47:34 +0530

    based on data returned

--- app.test.js
+++ app.test.js
@@ -1,9 +1,31 @@
-const request = require('supertest');
-const app = require('./app.js')
+const request = require("supertest");
+const app = require("./app.js");
 
 describe("TODOs API", () => {
-  it('GET /todos --> array todos', () => {})
-  it('GET /todos --> specific todo by ID', () => {})
-  it('POST /todos --> created todos', () => {})
-  it('GET /todos --> validates request body', () => {})
-})
+  it("GET /todos --> array todos", () => {
+    return request(app)
+      .get("/todos")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              name: expect.any(String),
+              completed: expect.any(Boolean),
+            }),
+          ])
+        );
+      });
+  });
+
+  it("GET /todos/id --> specific todo by ID", () => {
+    return request(app).get('/todos/123123').expect(404)
+  });
+
+  it("GET /todos/id --> 404 if not found", () => {});
+
+  it("POST /todos --> created todos", () => {});
+
+  it("GET /todos --> validates request body", () => {});
+});
